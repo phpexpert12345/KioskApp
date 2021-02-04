987bc0bfc5a86c34205e41137047b130a8b35b0f
Mohit Hooda <mohit@phpexperttechnologoies.com>
2021-02-04 11:49:19 +0530

    Fixed some issues in extra items

---
+++ app/src/main/java/com/phpexperts/kioskapp/Adapters/ExtraToppingAdapter.kt
@@ -0,0 +1,23 @@
+package com.phpexperts.kioskapp.Adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ExtraToppingAdapter :RecyclerView.Adapter<ExtraToppingAdapter.ExtraToppingViewHolder>(){
+    public class ExtraToppingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExtraToppingViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ExtraToppingViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
